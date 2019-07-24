1a687d7f2691d331fb6a33c8428b7cb35f58f195
Haves Irfansyah <havesirfan@ce.student.pens.ac.id>
2019-07-25 01:57:55 +0700

    Change from VB to C# (correction)

--- PIGEON_Desktop_WPF/PIGEON_Desktop_WPF/MainWindow.xaml.vb
+++ PIGEON_Desktop_WPF/PIGEON_Desktop_WPF/MainWindow.xaml.vb
@@ -1,3 +1,5 @@
 ﻿Class MainWindow
-
+    Private Sub moveWindow(sender As Object, e As DragEventArgs)
+        Console.WriteLine("This is dragEnter")
+    End Sub
 End Class
---
+++ Pigeon_WPF_cs/Pigeon_WPF_cs/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pigeon_WPF_cs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //rect_flight.Visibility = Visibility.Hidden;
+        }
+
+        private void clickedWindow(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+            Console.WriteLine("Window clicked");
+        }
+
+        private void popRight(object sender, MouseEventArgs e)
+        {
+            Button the_btn = (Button)sender;
+            Thickness pop = new Thickness(25,0,0,0);
+            switch (the_btn.Name)
+            {
+                case "btn_flight":
+                    icon_flight.Margin = pop;
+                    break;
+                case "btn_map":
+                    icon_map.Margin = pop;
+                    break;
+                case "btn_stats":
+                    icon_stats.Margin = pop;
+                    break;
+                case "btn_track":
+                    icon_track.Margin = pop;
+                    break;
+            }
+        }
+
+        private void normal(object sender, MouseEventArgs e)
+        {
+            ContentControl the_btn = (Button)sender;
+            Thickness back = new Thickness(25, 0, 0, 0);
+            
+        }
+    }
+}
