b9f00328d4dbc68989aa83b3db392cd66adc92d1
Haves Irfansyah <havesirfan@ce.student.pens.ac.id>
2019-07-25 19:21:28 +0700

    -Exit popup added -more status icons -tab control added

--- Pigeon_WPF_cs/Pigeon_WPF_cs/MainWindow.xaml.cs
+++ Pigeon_WPF_cs/Pigeon_WPF_cs/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,10 +21,11 @@ namespace Pigeon_WPF_cs
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool connected = false;
         public MainWindow()
         {
             InitializeComponent();
-            //rect_flight.Visibility = Visibility.Hidden;
+            icon_bat_1.Source = new CroppedBitmap(new BitmapImage(new Uri("pack://application:,,,/Resources/icons/bat-full.png")), new Int32Rect(0,0,400,396));
         }
 
         private void clickedWindow(object sender, MouseButtonEventArgs e)
@@ -35,7 +37,8 @@ namespace Pigeon_WPF_cs
         private void popRight(object sender, MouseEventArgs e)
         {
             Button the_btn = (Button)sender;
-            Thickness pop = new Thickness(25,0,0,0);
+            Thickness pop = new Thickness(25,5,5,5);
+            the_btn.Background = Brushes.Black;
             switch (the_btn.Name)
             {
                 case "btn_flight":
@@ -55,9 +58,40 @@ namespace Pigeon_WPF_cs
 
         private void normal(object sender, MouseEventArgs e)
         {
-            ContentControl the_btn = (Button)sender;
-            Thickness back = new Thickness(25, 0, 0, 0);
-            
+            Button the_btn = (Button)sender;
+            Thickness back = new Thickness(5, 5, 5, 5);
+            the_btn.Background = Brushes.Transparent;
+            switch (the_btn.Name)
+            {
+                case "btn_flight":
+                    icon_flight.Margin = back;
+                    break;
+                case "btn_map":
+                    icon_map.Margin = back;
+                    break;
+                case "btn_stats":
+                    icon_stats.Margin = back;
+                    break;
+                case "btn_track":
+                    icon_track.Margin = back;
+                    break;
+            }
+        }
+
+        private void closeApp(object sender, RoutedEventArgs e)
+        {
+            BlurEffect theblur= new BlurEffect();
+            theblur.Radius = 10;
+            Effect = theblur;
+            exitPop exitPop = new exitPop(appCheck());
+            exitPop.Owner = this;
+            exitPop.ShowDialog();
+        }
+        private byte appCheck()
+        {
+            byte code = 0;
+            if (connected) code = 1;
+            return code;
         }
     }
 }
---
+++ Pigeon_WPF_cs/Pigeon_WPF_cs/exitPop.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pigeon_WPF_cs
+{
+    /// <summary>
+    /// Interaction logic for exitPop.xaml
+    /// </summary>
+    public partial class exitPop : Window
+    {
+        private byte exitCode;
+        public exitPop(byte theCode = 0)
+        {
+            InitializeComponent();
+            exitCode = theCode;
+        }
+
+        private void batal(object sender, RoutedEventArgs e)
+        {
+            Owner.Effect = null;
+            Close();
+        }
+
+        private void keluar(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
