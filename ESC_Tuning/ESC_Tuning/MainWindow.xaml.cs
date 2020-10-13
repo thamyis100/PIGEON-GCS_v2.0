22dc4cfca9fbc8ae3a7149dc108de718a6ddd242
Haves Irfansyah <havesirfan@ce.student.pens.ac.id>
2020-10-13 08:56:16 +0700

    ESC tuning window layout

--- ESC_Tuning/ESC_Tuning/MainWindow.xaml.cs
+++ ESC_Tuning/ESC_Tuning/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +20,57 @@ namespace ESC_Tuning
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window//, INotifyPropertyChanged
     {
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        //private string _currADCOffset = "0.01";
+        //public string currADCOffset
+        //{
+        //    get { return _currADCOffset; }
+        //    set
+        //    {
+        //        if (value != _currADCOffset)
+        //        {
+        //            _currADCOffset = value;
+        //            OnPropertyChanged("currADCOffset");
+        //        }
+        //    }
+        //}
+        //private string _currOC5Value = "0.01";
+        //public string currOC5Value
+        //{
+        //    get { return _currOC5Value; }
+        //    set
+        //    {
+        //        if (value != _currOC5Value)
+        //        {
+        //            _currOC5Value = value;
+        //            OnPropertyChanged("currOC5Value");
+        //        }
+        //    }
+        //}
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //currOC5Value = "test";
         }
+
+        private void changed(object sender, TextChangedEventArgs e)
+        {
+            Console.WriteLine("CHANGED");
+        }
+
+        //protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        //{
+        //    PropertyChangedEventHandler handler = this.PropertyChanged;
+        //    if (handler != null)
+        //    {
+        //        var e = new PropertyChangedEventArgs(propertyName);
+        //        handler(this, e);
+        //    }
+        //}
     }
 }
