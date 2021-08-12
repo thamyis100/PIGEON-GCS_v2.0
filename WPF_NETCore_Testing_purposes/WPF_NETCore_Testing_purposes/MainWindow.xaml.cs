d8b73f4a26839656a151751d21d0ade9c729c756
Haves Irfansyah <havesirfan@ce.student.pens.ac.id>
2021-08-12 20:10:10 +0700

    bersih-bersih dan mulai implementasi mavlink

--- WPF_NETCore_Testing_purposes/WPF_NETCore_Testing_purposes/MainWindow.xaml.cs
+++ WPF_NETCore_Testing_purposes/WPF_NETCore_Testing_purposes/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿/*#define USEDEBUG*/
-
+﻿using MavLinkNet;
 using MessagePack;
 using System;
 using System.Collections.Generic;
@@ -24,167 +23,67 @@ using WebSocketSharp.NetCore.Server;
 
 namespace WPF_NETCore_Testing_purposes
 {
+    public class UasData
+    {
+        public byte SystemID { get; set; }
+
+        public UasSysStatus SystemStatus { get; set; }
+        public UasAttitude Attitude { get; set; }
+        public UasGlobalPositionInt Navigation { get; set; }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        delegate void UpdateTBTextDelegate(string text);
-        private delegate void UpdateUiTextDelegate();
 
-        WebSocket websocket_client;
-        Cookie tes;
 
-        SerialPort sPort;
-        List<byte> sPortBytes;
-
-        [MessagePackObject]
-        public class FlightData
-        {
-            [Key(0)]
-            public ArraySegment<short> Gyro { get; set; }
-            [Key(1)]
-            public ArraySegment<short> Accel { get; set; }
-            [Key(2)]
-            public float Pitch { get; set; }
-            [Key(3)]
-            public float Roll { get; set; }
-        }
-
-        public FlightData flightData;
+        public string konten { get; set; } = "Contents here";
 
         public MainWindow()
         {
             InitializeComponent();
 
-            Loaded += MainWindow_Loaded;
+            DataContext = this;
 
-            return;
-
-            flightData = new FlightData();
-            sPortBytes = new List<byte>();
-
-#if USEDEBUG
-            flightData.Gyro = new short[3] { 1, -1, 1 };
-            flightData.Accel = new short[3] { 352, 525, 4521 };
-            flightData.Pitch = 0.1203f;
-            flightData.Roll = -0.231f;
-#else
-            flightData.Gyro = new short[3] { 0, 0, 0 };
-            flightData.Accel = new short[3] { 0, 0, 0 };
-            flightData.Pitch = 0;
-            flightData.Roll = 0;
-#endif
-
-            sPort = new SerialPort("COM6", 2000000, Parity.None, 8, StopBits.One);
-            sPort.DataReceived += SPort_DataReceived;
-            sPort.ReceivedBytesThreshold = 25;
-            sPort.Open();
-        }
-
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            PrepareWebSocketClient();
-        }
-
-        private void PrepareWebSocketClient()
-        {
-            websocket_client = new WebSocket("ws://localhost:27772?waw=waw1234&test=create");
-            //websocket_client = new WebSocket("wss://echo.websocket.org");
-            //websocket_client = new WebSocket("wss://traccar.tekat.co/api/socket");
-
-            websocket_client.OnError += Websocket_client_OnError;
-            websocket_client.OnClose += Websocket_client_OnClose;
-            websocket_client.OnMessage += Websocket_client_OnMessage;
-            websocket_client.OnOpen += Websocket_client_OnOpen;
-
-            if(websocket_client.IsSecure) websocket_client.SslConfiguration.EnabledSslProtocols = System.Security.Authentication.SslProtocols.None;
-            websocket_client.SetCredentials("device", "device", true);
-
-            //websocket_client.set
-
-            //tes = new Cookie("JSESSIONID", "node01gc6ctgsyh7s41fspc1hkku4do4998.node0");
-            //websocket_client.SetCookie(tes);
-
-            websocket_client.SetCookie(new Cookie("testosteron", "testosteron4"));
-            websocket_client.SetCookie(new Cookie("testosteron1", "testosteron5"));
-            websocket_client.SetCookie(new Cookie("testosteron2", "testosteron6"));
-
-            Debug.WriteLine("Try connecting to " + websocket_client.Url.ToString());
-            websocket_client.Connect();
-        }
-
-        private void Websocket_client_OnOpen(object sender, EventArgs e)
-        {
-            Debug.WriteLine("[CLIENT][CONNECTED]");
-        }
-
-        private void Websocket_client_OnMessage(object sender, MessageEventArgs e)
-        {
-            Debug.WriteLine("[CLIENT][NEW DATA]: ");
-            Debug.WriteLine(e.Data);
-            Dispatcher.Invoke(DispatcherPriority.Send, new UpdateTBTextDelegate(UpdateTB1), "[CLIENT][NEW DATA]: " + e.Data);
-
-            //PrintTB1("[CLIENT][NEW DATA]: " + e.Data);
-        }
-
-        private void Websocket_client_OnClose(object sender, CloseEventArgs e)
-        {
-            Debug.WriteLine("[CLIENT][CLOSED]: ");
-            Debug.WriteLine(e.Reason + " (" + e.Code.ToString() + ')');
-        }
-
-        private void Websocket_client_OnError(object sender, ErrorEventArgs e)
-        {
-            Debug.WriteLine("[CLIENT][ERROR]: ");
-            Debug.WriteLine(e.Message);
-        }
-
-        private void UpdateTB1(string text)
-        {
-            tb1.Text += text;
-        }
-
-        private void SPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
-            SerialPort usedPort = (SerialPort)sender;
-            while (usedPort.BytesToRead > 0)
+            UasData currUAV = new()
             {
-                sPortBytes.Add((byte)usedPort.ReadByte());
-                Debug.Write(sPortBytes.Last().ToString("X") + " ");
-            }
-            Debug.WriteLine("");
-
-            Dispatcher.Invoke(DispatcherPriority.Send, new UpdateUiTextDelegate(UpdateFlightData));
-        }
-
-        private void UpdateFlightData()
-        {
-            try
-            {
-                flightData = MessagePackSerializer.Deserialize<FlightData>(sPortBytes.ToArray());
-                sPortBytes.Clear();
-
-                tb1.Text = "Serialized data:\r\n";
-                foreach (var bite in MessagePackSerializer.Serialize(flightData))
+                SystemID = 67,
+                SystemStatus = new()
                 {
-                    tb1.Text += bite.ToString("X") + " ";
+                    BatteryRemaining = 80,
+                    DropRateComm = 15,
+                    Load = 10
+                },
+                Attitude = new() { Yaw = 25.0f, Pitch = 25.0f, Roll = 25.0f },
+                Navigation = new()
+                {
+                    Alt = 250,
+                    Hdg = (ushort)(16.25f * 182),
+                    Lat = int.Parse("-7.410753".Replace(".", "")),
+                    Lon = int.Parse("112.7042528".Replace(".", ""))
                 }
-                tb1.Text += "\r\n\r\n" + String.Format("Pitch: {0:F2}\r\nRoll: {1:F2}", flightData.Pitch, flightData.Roll);
-            }
-            catch (MessagePackSerializationException exc)
-            {
-                Debug.WriteLine(exc.Message);
-                if (sPortBytes.Count > 30) sPortBytes.Clear();
-            }
-        }
+            };
 
-        private void send_tb_keypressed(object sender, KeyEventArgs e)
-        {
-            if (e.Key.Equals(Key.Enter))
+            byte[][] messages = {
+                MavLinkPacket.GetBytesForMessage(currUAV.SystemStatus, currUAV.SystemID, (byte)MavComponent.MavCompIdAll, 0, 0),
+                MavLinkPacket.GetBytesForMessage(currUAV.Attitude, currUAV.SystemID, (byte)MavComponent.MavCompIdAll, 0, 0),
+                MavLinkPacket.GetBytesForMessage(currUAV.Navigation, currUAV.SystemID, (byte)MavComponent.MavCompIdAll, 0, 0)
+            };
+
+            konten = "";
+            foreach (var message in messages)
             {
-                websocket_client.Send(send_tb.Text);
-                send_tb.Text = "";
+                konten += "Message : [ ";
+                Debug.Write("Message : [ ");
+                foreach (var item in message)
+                {
+                    Debug.Write(item.ToString("X2") + ' ');
+                    konten += item.ToString("X2") + ' ';
+                }
+                Debug.WriteLine("]");
+                konten += "]\n";
             }
         }
     }
